2f0ecc9c9364410cd404263a810a973bdf084dcc
FaceNach <ignacioijg@gmail.com>
2025-07-02 19:00:00 -0300

    Second Commit

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,20 @@
+//Variables and constants
+
+const name ='Nacho';
+const surname = 'Gun';
+
+let valueDice = 5;
+valueDice = 4;
+
+console.log(name, surname, valueDice);
+
+if ( true ){
+
+    const name ="Peter";
+    valueDice = 6;
+    console.log(valueDice);
+    console.log(name);
+}
+
+console.log(valueDice);
+
