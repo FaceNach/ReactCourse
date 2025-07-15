e1fa2c59f55771f2166d21e986de17d48f2d37d6
FaceNach <ignacioijg@gmail.com>
2025-07-15 18:14:07 -0300

    Keep it going

---
+++ 04-gif-expert-app/src/components/AddCategory.jsx
@@ -0,0 +1,33 @@
+import {useState} from "react";
+
+
+export const AddCategory = ({setCategories}) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const onInputChange = (event) => {
+        setInputValue(event.target.value);
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if(inputValue.trim().length <= 1 ) return;
+        console.log(inputValue);
+
+        setCategories((categories) =>[inputValue, ...categories]);
+        setInputValue('');
+    }
+
+    return (
+        <form onSubmit={(event) => onSubmit(event)}  >
+            <input
+                type="text"
+                placeholder="Search Gifs"
+                value={inputValue}
+                onChange={(event) => onInputChange(event) }
+            />
+        </form>
+
+    )
+}
+
--- 04-gif-expert-app/src/components/GifExpertApp.jsx
+++ 04-gif-expert-app/src/components/GifExpertApp.jsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import {AddCategory} from "./AddCategory.jsx";
 
 const apiKey = '30sY6PVOWTph7CprqSM4vEmuUAwMJX2E'
 
@@ -6,11 +7,18 @@ export const GifExpertApp = () => {
 
     const[categories, setCategories] = useState(['One Punch', 'Dragon Ball'])
 
+    const onAddCategory = () => {
+        //setCategories(cat => [...cat,"Testing"]); another way to add
+        setCategories([ "Testing",...categories,] );
+    }
+
     console.log(categories)
 
     return (
         <>
             <h1>Hello from GifExpertApp</h1>
+
+            <AddCategory setCategories = {setCategories} />
 
 
             <ol>
